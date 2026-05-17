3edc0d8683ec0757f85e1a45cb7dc583e248e3e6
Ammar-AZ <ammar.zaghloul55@gmail.com>
2026-05-17 13:42:54 -0600

    Many to Many relationship mapped
    Verified tables in terminal, migrations tracked now

--- Models/ProjectMember.cs
+++ Models/ProjectMember.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace Tasked.Models;
+[PrimaryKey(nameof(ProjectId),nameof(UserId))]
 public class ProjectMember
 {
     public Guid ProjectId {get; set;}
+    public Project Project {get; set;} = null!;
     public Guid UserId {get; set;}
+    public User User {get; set;} = null!;
     //public enum Role {get; set;} not nullable, roles not decided yet   
 }
